8032b8c1b9802512e3bcaeead544c5ab4709266d
Hunt The Alien <36826771+lalenguanegra@users.noreply.github.com>
2020-09-06 09:07:32 -0700

    Update sendMp3.py

--- sendMp3.py
+++ sendMp3.py
@@ -1,6 +1,5 @@
 @bot.command()
-async def send(ctx):
+async def email(ctx):
     await ctx.send("Sending...")
-    area=ctx.message.channel
-    await channel.send(area, file=discord.File(' 1.mp3'))
+    await ctx.message.channel.send(file=discord.File(' 1.mp3'))
     await ctx.send("Sent!")
