ac3ae2f4b1f2462c020f9d374968c45c0d9e33b8
Hunt The Alien <36826771+lalenguanegra@users.noreply.github.com>
2020-09-06 09:52:43 -0700

    Create Roles.py

---
+++ Roles.py
@@ -0,0 +1,36 @@
+import discord
+import sys
+import subprocess 
+from subprocess import call
+from discord.ext import commands
+bot = commands.Bot(command_prefix='!')
+
+@bot.command()
+@commands.has_role('ayylmao')
+async def ayy(ctx, arg):
+    await ctx.send("Please Wait...")
+    text_file = open("URL.txt", "a")  
+    text_file.write(arg+"\n")
+    await ctx.send(arg + " was added!")
+
+
+@bot.command()
+@commands.has_role('ayylmao')
+async def go(ctx):
+    await ctx.send("Loading...")
+    subprocess.call("YT.py", shell=True)
+    await ctx.send("Downloaded!")
+
+@bot.command()
+@commands.has_role('ayylmao')
+async def send(ctx):
+    await ctx.send("Sending...")
+    await ctx.message.channel.send(file=discord.File(' 1.mp3'))
+    await ctx.send("Sent!")
+
+@bot.command()
+@commands.has_role('ayylmao')
+async def gone(ctx):
+    await ctx.send("Deleted...")
+    subprocess.call("DEL.py", shell=True)
+bot.run('TOKEN')
